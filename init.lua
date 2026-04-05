5c0c804738cc6ea300d5efb399fb95a158772c06
ilyaivanov <static.ila@gmail.com>
2026-04-05 10:32:43 +0300

    add nvimtree file explorer

--- init.lua
+++ init.lua
@@ -193,6 +193,7 @@ vim.keymap.set('n', ']d', function()
   vim.defer_fn(function()
     vim.diagnostic.open_float(nil, {})
   end, 50)
+
 end)
 
 vim.keymap.set('n', '[d', function()
@@ -202,3 +203,42 @@ vim.keymap.set('n', '[d', function()
   end, 50)
 end)
 
+
+
+-- File Explorer 
+vim.pack.add({
+  "https://github.com/nvim-tree/nvim-web-devicons",
+  "https://github.com/nvim-tree/nvim-tree.lua"
+})
+
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with a config
+
+---@type nvim_tree.config
+local config = {
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+}
+require("nvim-tree").setup(config)
+
+vim.keymap.set("n", "<leader>to", "<cmd>NvimTreeOpen<CR>")
